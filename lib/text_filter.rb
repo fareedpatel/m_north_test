8114d1b2345d7b642c158d4741feb4cdc4893441
Fareed Patel <fareed_patel@hotmail.co.uk>
2016-09-22 08:18:58 +0100

    user stories done - tests passing

--- lib/text_filter.rb
+++ lib/text_filter.rb
@@ -1,9 +1,31 @@
 class TextFilter
-  attr_reader :string_input, :banned_word
+  attr_reader :string_input, :banned_words
 
   def initialize(string_input: string_input)
     @string_input = string_input
-    @banned_word = BannedWord.new
   end
+
+  def banned_subtitute(string_input, banned_words = %w(red green yellow blue))
+    banned_words.map!(&:downcase)
+
+    string_input.split.map do |word|
+        if includes_punctiation?(word)
+          include_punctiation = word.chars.last
+          word = word[0..-2]
+          banned_words.include?(word.downcase) ? 
+          word.gsub(/[aeiou]/i,"-") + include_punctiation : word + include_punctiation
+        else
+          banned_words.include?(word.downcase) ? word.gsub(/[aeiou]/i,"-") : word
+        end
+      end.join(" ")
+  end
+
+
+private
+
+  def includes_punctiation?(word)
+    word.match(/\W$/).nil? ? false : true
+  end
+
 
 end
--- spec/text_filter_spec.rb
+++ spec/text_filter_spec.rb
@@ -3,7 +3,11 @@ require 'text_filter'
 describe TextFilter do
   subject(:text_filter) { described_class.new( string_input: string_input) }
 
-  let(:string_input) { 'This is a green and evergreen greenbelt' }
+  let(:string_input)    { 'my favourite colours are green and blue' }
+  let(:string_input_2)  { 'I would like red, blue and green clothes' }
+  let(:string_input_3)  {'No houses on the greenbelt'}
+  let(:banned_words)    { 'red green blue yellow' }
+  let(:exceptions_list) { 'covered coloured evergreen greenbelt blues' }
 
     describe 'String input' do
       it 'allows you to pass string and initalize' do
@@ -11,9 +15,29 @@ describe TextFilter do
       end
     end
 
-    describe 'BannedWords' do
-      it 'can be assigned when initialized' do
-        expect(text_filter.banned_word).to be_instance_of BannedWord
+    describe 'Banned Subtitute' do
+
+      it 'returns r-d when string input is red' do
+        expect(text_filter.banned_subtitute('red')).to eq "r-d"
+      end
+
+      it 'returns bl-- when string input is blue' do
+        expect(text_filter.banned_subtitute('blue')).to eq "bl--"
+      end
+
+      it "replaces vowels of banned words" do
+        expect(text_filter.banned_subtitute(string_input)).to eq "my favourite colours are gr--n and bl--"
+      end
+
+      it 'returns the string with the punction' do
+          expect(text_filter.banned_subtitute(string_input_2)).to eq "I would like r-d, bl-- and gr--n clothes"
       end
     end
+
+    describe 'Exceptions List' do
+      it 'does not alter words part of the exceptions list' do
+        expect(text_filter.banned_subtitute(string_input_3)).to eq "No houses on the greenbelt"
+    end
+  end
+
 end
